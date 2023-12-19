dd56be99433b8b16dfeabd43ddc139ad68db57d3
Nrushank Bodar <nrushankbodar4490@gmail.com>
2023-12-19 19:05:04 +0530

    recreate_datatbase

--- store/models.py
+++ store/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class Customer(models.Model):
     first_name= models.CharField(max_length=50)
     last_name= models.CharField(max_length = 50)
     email = models.EmailField(unique = True)
@@ -18,12 +18,12 @@ class Restaurant(models.Model):
 class Menu(models.Model):
     title= models.CharField(max_length= 255)
     restaurant = models.ForeignKey(Restaurant,on_delete=models.CASCADE)
-    descreption = models.CharField(max_length = 255)
+    description = models.CharField(max_length = 255)
 
-class MenuItems(models.Model):
+class Items(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length= 255)
-    price = models.DecimalField(max_digits=4,decimal_places= 2)
+    price = models.DecimalField(max_digits=5,decimal_places= 2)
     menu = models.ForeignKey(Menu,on_delete=models.CASCADE)
 
 class Order(models.Model):
@@ -32,13 +32,25 @@ class Order(models.Model):
         ('in_progress','In Progress'),
         ('deliverd','Deliverd')
     ]
-    user = models.ForeignKey(User,on_delete= models.CASCADE)
-    order_date= models.DateField(auto_now_add = True)
+    customer = models.ForeignKey(Customer,on_delete= models.CASCADE)
+    items = models.ManyToManyField(Items)
+    total_price = models.DecimalField(max_digits=5,decimal_places = 2)
     status = models.CharField(max_length=20,choices = STATUS_CHOICES,default='placed')
+    created_at = models.DateTimeField()
+    
+    def calculate_total_price(self):
+       total_price = sum(item.price for item in self.items.all())
+       self.total_price = total_price
+       self.save()
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.calculate_total_price()
+
 
 class OrderItem(models.Model):
     order_id = models.ForeignKey(Order,on_delete=models.CASCADE)
-    item_id = models.ForeignKey(MenuItems,on_delete= models.CASCADE)
+    item_id = models.ForeignKey(Items,on_delete= models.CASCADE)
     quantity = models.PositiveIntegerField()
 
 
--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Customer
 
 # Create your views here.
 
 def say_hello(request):
+    user =Customer.objects.get(pk=5)
+
+
     return render(request,'hello.html',{'name':'nrushank'})
